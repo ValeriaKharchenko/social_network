220978cebb01055db0ebddfd055901525539e4c8
Valeria Kharchenko <valerie.kharchenko@gmail.com>
2022-04-19 19:39:45 +0300

    some fix

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,47 +1,39 @@
-import {useForm} from "react-hook-form";
-import userService from "../utilites/user-service"
+import { useForm } from "react-hook-form";
+import userService from "../utilites/user-service";
 
 export default function Login() {
-    interface FormInput {
-        email: string;
-        password: string;
+  interface FormInput {
+    email: string;
+    password: string;
+  }
+  const { handleSubmit, register } = useForm<FormInput>();
+  const onSubmit = async (data: FormInput) => {
+    try {
+      console.log(data);
+      await userService.login(data.email, data.password);
+    } catch (e) {
+      if (e instanceof Error) {
+        console.log(e.message);
+        alert(e.message);
+      } else {
+        console.log(e);
+      }
     }
-    const us = new userService();
-    const {
-        handleSubmit, register
-    }
-        = useForm<FormInput>();
-
-    const onSubmit = async (data: FormInput) => {
-
-        try {
-            console.log(data);
-            await us.login(data.email, data.password);
-        } catch (e) {
-            console.log("My error: ", e);
-            alert(e);
-        }
-    }
-    return (
+  };
+  return (
+    <div>
+      <h3>Login</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-            <h3>Login</h3>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label>E-mail</label>
-                    <input type={'text'}
-                           {...register("email")}
-                           required
-                    /></div>
-                <div>
-                    <label>Password</label>
-                    <input type={'password'}
-                           {...register("password")}
-                           required
-                    />
-                </div>
-                <button type={"submit"}>Login</button>
-            </form>
+          <label>E-mail</label>
+          <input type={"text"} {...register("email")} required />
         </div>
-
-    );
+        <div>
+          <label>Password</label>
+          <input type={"password"} {...register("password")} required />
+        </div>
+        <button type={"submit"}>Login</button>
+      </form>
+    </div>
+  );
 }
--- src/utilites/user-service.ts
+++ src/utilites/user-service.ts
@@ -1,14 +1,11 @@
 import http from "./http-common"
 
-class userService {
-    async login (email:string, pwd:string){
+export default {
+    async login(email: string, pwd: string) {
         try {
-            await http.post("login", { credential: email, password: pwd });
+            await http.post("login", {credential: email, password: pwd});
         } catch (err) {
-            // console.log("Here", err);
             throw err;
         }
     }
 }
-
-export default userService;
