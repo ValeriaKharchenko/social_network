4fac57fbf0450f410628117b0cab6fc7f30845e8
Valeria Kharchenko <valerie.kharchenko@gmail.com>
2022-04-26 13:34:15 +0300

    redux storage added

---
+++ src/store/store.tsx
@@ -0,0 +1,51 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import userReducer from "./userSlice";
+
+const saveToLocalStorage = (state: any) => {
+  // console.log("state:", state);
+  try {
+    localStorage.setItem("userInfo", JSON.stringify(state));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const stateStr = localStorage.getItem("userInfo");
+    return {
+      user: {
+        userInfo: stateStr
+          ? JSON.parse(stateStr)
+          : {
+              name: "",
+              email: "",
+              id: "",
+              isAuthorised: false,
+            },
+        pending: null,
+        error: false,
+      },
+    };
+  } catch (e) {
+    console.error(e);
+    return undefined; //trow error?
+  }
+};
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistedStore = loadFromLocalStorage();
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: persistedStore,
+});
+
+store.subscribe(() => {
+  saveToLocalStorage(store.getState().user.userInfo);
+});
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
