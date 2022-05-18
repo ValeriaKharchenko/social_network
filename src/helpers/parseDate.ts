6e74c59cd790c90db537c91c4dc6c8ee28f32b73
Valeria Kharchenko <valerie.kharchenko@gmail.com>
2022-05-18 19:17:20 +0300

    parse data move to helpers

--- src/helpers/parseDate.ts
+++ src/helpers/parseDate.ts
@@ -5,5 +5,5 @@ export function parseDate(str: string): string {
   const day = (date.getDate() < 10 ? "0" : "") + date.getDate();
   const month = (date.getMonth() + 1 < 10 ? "0" : "") + (date.getMonth() + 1);
   const year = date.getFullYear();
-  return day + "/" + month + "/" + year + " " + min + ":" + hour;
+  return day + "/" + month + "/" + year + " " + hour + ":" + min;
 }
