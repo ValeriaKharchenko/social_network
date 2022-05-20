c8d033db7d887ac9360838cc23891d827b6510e9
Silver Luhtoja <silver.luhtoja@gmail.com>
2022-05-20 12:47:03 +0300

    request components , event going handler, fixed invite btn , automatic update on page (not all)

--- src/components/groups/GroupEvents.jsx
+++ src/components/groups/GroupEvents.jsx
@@ -1,4 +1,5 @@
 import {useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 import GroupService from "../../utilities/group_service"
 import SingleGroupEvent from "./SingleGroupEvent"
 
@@ -6,10 +7,14 @@ import SingleGroupEvent from "./SingleGroupEvent"
 const GroupEvents = ({id}) => {
   const [events,setEvents] = useState([])
   const group_service = GroupService()
+  const storeInfo = useSelector(state => state)
 
   useEffect(()=>{
     group_service.getGroupEvents(id).then(res => {setEvents(res)})
-  },[id])
+    group_service.getJoinedEvents()
+
+    console.log("REFRESHING EVENTS");
+  },[id,storeInfo.groups.updateStatus])
 
   return (
     <div>
---
+++ src/components/groups/RequestList.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+import GroupService from "../../utilities/group_service"
+import Request from "./Request"
+const Requests = () => {
+    const group_service = GroupService()
+    const [list,setList] = useState([])
+    const storeInfo = useSelector(state => state)
+    let {id} = useParams()
+
+    useEffect(()=>{
+        group_service.getJoinRequests(id).then(res => { setList(res)})
+    },[storeInfo.groups.updateStatus])
+
+    return (
+        <div>
+            {!!list ?    
+            <div>
+                {list.map(user => (<Request key={user.user_id}  data={user} />))}
+            </div>
+            :
+             <div>No new group join requests</div>
+            }
+        </div>
+    )
+}
+
+export default Requests
